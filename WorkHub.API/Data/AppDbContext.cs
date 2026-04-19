0921df7b67813c31eb7c8192e614a940400aed0f
Abdullah Matin <abdullah.matin6910@gmail.com>
2026-04-19 13:05:47 +0600

    Day 1: Setup .NET 9 Web API, PostgreSQL, EF Core, initial migration

---
+++ WorkHub.API/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WorkHub.API.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+    // DbSets added on Day 5
+}
